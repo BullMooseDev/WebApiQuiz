9d752b2ec8fb2441e8164fbc35159a01adac6d72
thatblondetitan <koleervine@gmail.com>
2022-01-16 13:01:36 -0700

    readme changes
    fixed readme for deployed application

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -117,3 +117,16 @@ add event listener for the submit button
 create a timer for the quiz
 create a way for wrong answers to deduct time from the quiz
 create a display for how many answers were correct in the end */
+
+
+/* GIVEN I am taking a code quiz
+WHEN I click the Start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score */
