562faaa4fea2b830177c780ff10540652aa617dc
thatblondetitan <koleervine@gmail.com>
2022-01-18 18:38:41 -0700

    html changes
    fixed some html pathing

--- Assets/JS/end.js
+++ Assets/JS/end.js
@@ -1,13 +1,13 @@
 const username = document.querySelector("#username");
 const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const finalScore = document.querySelector("#finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
+let mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innterText = mostRecentScore;
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value
@@ -25,11 +25,10 @@ saveHighScore = e => {
 
     highScores.sorts((a,b) => {
         return b.score - a.score
-    });
+    })
 
     highScores.splice(5)
 
     localStorage.setItem("highScores", JSON.stringify(highScores))
     window.location.assign("/");
-}
-
+};
